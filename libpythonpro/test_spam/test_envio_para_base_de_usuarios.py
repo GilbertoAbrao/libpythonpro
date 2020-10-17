5bb3ded7e645ad1cef09c0fed580307a494e35ee
Gilberto Filho <ggafbr@gmail.com>
2020-10-17 18:03:07 -0400

    Alterado o gerenciador de dependencias para o pipenv

--- libpythonpro/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/test_spam/test_envio_para_base_de_usuarios.py
@@ -2,7 +2,15 @@ from libpythonpro.spam.enviador_de_email import Enviador
 from libpythonpro.spam.main import EnviadorDeSpam
 
 
-def test_envio_de_spam(sessao):
+def test_qtd_de_spam(sessao):
+    enviador_de_spam = EnviadorDeSpam(sessao, Enviador())
+    enviador_de_spam.enviar_emails(
+        'ggafbr@gmail.com',
+        'Curso Python Pro',
+        'Confira os modulos fantásticos'
+    )
+
+def test_parametros_de_spam(sessao):
     enviador_de_spam = EnviadorDeSpam(sessao, Enviador())
     enviador_de_spam.enviar_emails(
         'ggafbr@gmail.com',
--- libpythonpro/test_spam/test_usuarios.py
+++ libpythonpro/test_spam/test_usuarios.py
@@ -2,13 +2,14 @@ from libpythonpro.spam.modelos import Usuario
 
 
 def test_salvar_usuario(sessao):
-    usuario = Usuario(nome='Gilberto')
+    usuario = Usuario(nome='Gilberto', email='ggafbr@gmail.com')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 def test_listar_usuario(sessao):
-    usuarios = [Usuario(nome='Gilberto'), Usuario(nome='Kaka')]
+    usuarios = [Usuario(nome='Gilberto', email='ggafbr@gmail.com'),
+                Usuario(nome='Kaka', email='ggafbr@gmail.com')]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
